eb89b37c371c5feff637ebdbee448a00978a057b
SolidSnake26 <solid.snake26@gmail.com>
2024-07-24 23:29:07 +0400

    Commit Home Wednesday_24-Jul-2024 23:29

--- update.git.ps1
+++ update.git.ps1
@@ -1,5 +1,8 @@
 git add --all
 Write-Host "All Added"
-git commit -m "Commit Office $(Get-Date -Format "dddd_dd-MMM-yyyy HH:mm")"
+[string]$WS=$null
+if ($env:COMPUTERNAME -eq '5CD04794WV') {$WS='Home'}
+else {$WS='Office'}
+git commit -m "Commit $WS $(Get-Date -Format "dddd_dd-MMM-yyyy HH:mm")"
 Write-Host "All Added"
 git push -u origin master
